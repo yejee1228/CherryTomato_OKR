5b2efd3b7782f0f3d561756dd2959fbe912654d6
yejee1228 <yejee1228@gmail.com>
2022-08-12 23:59:27 +0900

    기능구현

--- frontend/pages/account/findPassword/changePassword/index.tsx
+++ frontend/pages/account/findPassword/changePassword/index.tsx
@@ -1,16 +1,53 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 import { Header } from 'components/account';
 import * as A from 'lib/styles/accountStyle';
+import * as RegExp from 'lib/hooks/useRegExp';
+import { useRouter } from 'next/router';
 
 const Index = () => {
+    const router = useRouter()
     const [passWordType, setPassWordType] = useState('password')
-    const [passwd, setPasswd] = useState('')
-    const [checkPasswd, setCheckPasswd] = useState('')
+    const [inputs, setInputs] = useState({
+        password: '',
+        passwordCheck: ''
+    })
+    const { password, passwordCheck } = inputs
+    const [passwordError, setPassWordError] = useState('')
 
     const changePwType = () => {
         passWordType === 'password' ? setPassWordType('text') : setPassWordType('password')
 
+    }
+
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setInputs({
+            ...inputs,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        if (password !== '' && !RegExp.checkRegExp(password, RegExp.passwordRegExp)) {
+            setPassWordError('영문/숫자/특수문자 중 2가지 이상 조합하여 6~12자로 입력해주세요.')
+        } else if (passwordCheck !== '' && password !== passwordCheck) {
+            setPassWordError('비밀번호가 일치하지 않습니다.')
+        } else {
+            setPassWordError('')
+        }
+    }, [password, passwordCheck])
+    const submit = () => {
+        if (password == '') {
+            setPassWordError('비밀번호를 입력해주세요.')
+            return
+        } else if (passwordCheck == '') {
+            setPassWordError('비밀번호를 한번 더 입력해주세요.')
+            return
+        } else {
+            router.push('./complete')
+            return//비밀번호 변경 api
+        }
     }
 
     return (
@@ -25,15 +62,16 @@ const Index = () => {
                 </A.SubTitleBox>
                 <A.InputWrapMT>
                     <A.InputBox>
-                        <A.Input type={passWordType} name='passwd' value={passwd} placeholder='영문/숫자/특수문자 중 2가지 이상 조합, 6~12자' />
+                        <A.Input type={passWordType} name='password' value={password} placeholder='영문/숫자/특수문자 중 2가지 이상 조합, 6~12자' onChange={inputHandler} />
                         {passWordType === 'password' ? <A.InputIcon onClick={changePwType}><AiFillEyeInvisible /></A.InputIcon> : <div onClick={changePwType}><AiFillEye /></div>}
                     </A.InputBox>
                     <A.InputBox>
-                        <A.Input type={passWordType} name='checkPasswd' value={checkPasswd} placeholder='새 비밀번호 확인' />
+                        <A.Input type={passWordType} name='passwordCheck' value={passwordCheck} placeholder='새 비밀번호 확인' onChange={inputHandler} />
                         {passWordType === 'password' ? <A.InputIcon onClick={changePwType}><AiFillEyeInvisible /></A.InputIcon> : <div onClick={changePwType}><AiFillEye /></div>}
+                        <A.AlertBox about='error'>{passwordError}</A.AlertBox>
                     </A.InputBox>
                 </A.InputWrapMT>
-                <A.RedButton>
+                <A.RedButton onClick={submit}>
                     <A.RedButtonSpan>
                         확인
                     </A.RedButtonSpan>
